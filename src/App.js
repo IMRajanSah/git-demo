9b24151904c9fc2d5ae8ff748eed1affd1c6b606
Rajan Kumar Sah <rajansah369@gmail.com>
2021-07-02 11:43:52 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment=()=>{
+    setCount(count+1)
+  }
+  
+  const decrement=()=>{
+    if(count>0)
+    setCount(count-1);
+  }
+  const Zero=()=>{
+    setCount(0)
+  }
+  const mystyle={
+    fontSize:30
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br></br>
+      <center><h2>Counter App</h2>
+      <p style={mystyle}>{count}</p>
+    <button onClick={ increment}>Increase Me</button>&nbsp;&nbsp;
+    <button onClick={decrement}>Decrease Me</button>&nbsp;&nbsp;
+    <button onClick={Zero}>Set Me Zero</button></center>
     </div>
   );
 }
